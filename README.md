65c64038ef0067ddbe20c11b3c6d8fd84661121f
Daniel Lu <dandydanny@gmail.com>
2014-01-30 17:06:51 -0600

    Working AJAX solution

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. Download this [application skeleton](http://cl.ly/1l1F3N2I3E3x).
 2. Convert the app to use AJAX.
 3. Add any files you changed to your gist and submit your code. 	
+
+# List of modified files:
+
+1.
+2. 
+4. 
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+	console.log("start js")
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
@@ -7,4 +7,38 @@ $(document).ready(function () {
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
+  // Binds to "Roll Dice" form submit
+  $( "form" ).submit(function( event ) {
+
+  	// Debug
+  	console.log("detected form submit");
+
+  	// Do AJAX post, method="post" action="/rolls"
+  	$.post("/rolls");
+
+  	// Generate a random number between 1~6
+  	var rand = Math.floor((Math.random()*6)+1);
+  	console.log(rand);
+		
+  	//replace the contents of the "#die" DIV
+  	$( "#die" ).html( "<img src='" + rand+ ".png' title='"+ rand + "' alt='the roll'>");
+  	$( "#die" ).css({ opacity: 0})
+		$( "#die" ).fadeTo( "slow", 1)
+  	// Prevent default POST action
+  	event.preventDefault();
+	});
+
+	$( "form" ).submit(function( event ) {
+	  if ( $( "input:first" ).val() === "correct" ) {
+	    $( "span" ).text( "Validated..." ).show();
+	    return;
+	  }
+	 
+	  $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
+	  event.preventDefault();
+	});
+
+
+
 });
+		
