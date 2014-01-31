77b8569a31189d727a7d75e8d4f0c3db0ffa0183
Apprentice <apprentice@dbc-chicago-6.local>
2014-01-31 06:42:39 -0600

    Styling changes

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,27 @@
+// function fadeAll(elems) {
+//     console.log("called fade fn");
+//     $("div").filter('hidden').fadeIn(500, function() { fadeAll(elems); });
+// }
+
 $(document).ready(function () {
-	console.log("start js")
+	console.log("start js");
+//   $( "div" ).css( "visibility", "hidden" );
+
+//   $( "div" ).css( "visibility", "hidden" );
+//   // animations:
+
+
+//   setTimeout(function() {
+//         fadeAll($('div'));
+//   }, 300);
+
+  // $( ".container" ).css({ opacity: 0});
+  // $( ".container" ).fadeTo( 500, 1);
+  // $( "h1" ).css({ opacity: 0});
+  // $( "h1" ).fadeTo( 1000, 1);
+  // $( "input" ).css({ opacity: 0});
+  // $( "input" ).fadeTo( 2000, 1);
+
   // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
@@ -19,7 +41,7 @@ $(document).ready(function () {
   	// Generate a random number between 1~6
   	var rand = Math.floor((Math.random()*6)+1);
   	console.log(rand);
-		
+
   	//replace the contents of the "#die" DIV
   	$( "#die" ).html( "<img src='" + rand + ".png' title='"+ rand + "' alt='the roll'>");
   	$( "#die" ).css({ opacity: 0})
@@ -32,4 +54,4 @@ $(document).ready(function () {
   	event.preventDefault();
 	});
 });
-		
+
